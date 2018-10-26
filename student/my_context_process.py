84aea97d8aec21d0b9eecb9ccda151ee80be61da
wangxiaowan <1061209760@qq.com>
2018-10-26 22:27:40 +0800

    时间分类

--- student/my_context_process.py
+++ student/my_context_process.py
@@ -1,14 +1,14 @@
 #coding=utf-8
-from django.db.models import Count, connection
+from django.db import connection
 
 from models import *
 def rightInfo(request):
     #分类
-    cate_post = Post.objects.values('category__cname', 'category').annotate(c=Count('*')).order_by('-c')
+    # cate_post = Post.objects.values('category__cname', 'category').annotate(c=Count('*')).order_by('-c')
 
-    #归档
+    #按时间归档
     cursor = connection.cursor()
     cursor.execute("SELECT created,COUNT('*') AS c FROM blog_post GROUP BY DATE_FORMAT(created,'%Y%m') ORDER BY c DESC")
     archive = cursor.fetchall()
 
-    return {'cate_post': cate_post,'archive':archive}
+    return {'archive':archive}
--- system/settings.py
+++ system/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'student.my_context_process.rightInfo'
 ]
 
 ROOT_URLCONF = 'system.urls'
