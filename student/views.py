7652a52412ef9336871f65af6542c715924691f6
wangxiaowan <1061209760@qq.com>
2018-10-26 15:11:33 +0800

    近期要闻的css\js

--- student/views.py
+++ student/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from models import *
 
 # Create your views here.
 def index_view(request):
@@ -13,11 +14,12 @@ def all_meuns(request):
     return render(request,'all_menus.html')
 
 
+def c_news(request):
+    posts=Post.objects.all()
+    return render(request,'c_news.html',{'posts':posts})
+
 def c_manage(request):
     return render(request,'c_manage.html')
-
-def c_news(request):
-    return render(request,'c_news.html')
 
 
 def c_classify(request):
