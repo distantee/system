c88aa4bc6b1a667edf7ffb61debcef82edb17b93
wangxiaowan <1061209760@qq.com>
2018-10-26 17:10:47 +0800

    详情页

--- student/urls.py
+++ student/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     url(r'^index/',views.index_view,name='index'),
     url(r'^all_menus/',views.all_meuns),
     url(r'^c_manage/', views.c_manage),
+    # url(r'^page/(\d+)',views.c_manage),
+    #详情页路由
+    url(r'^post/(\d+)', views.post_view),
     url(r'^c_news/', views.c_news),
     url(r'^c_classify/', views.c_classify),
 ]
--- student/views.py
+++ student/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 from models import *
@@ -14,13 +15,42 @@ def all_meuns(request):
     return render(request,'all_menus.html')
 
 
-def c_news(request):
-    posts=Post.objects.all()
-    return render(request,'c_news.html',{'posts':posts})
+def page(num,size):
+    # print num,size
+    num=int(num)
+    paginator=Paginator(Post.objects.all().order_by('-created'),size)
+    # print paginator.count 总的记录条数
+    # print paginator.num_pages 总的分页数
+    # print paginator.page(num) 显示当前页所包含的对象
+    if num<1:
+        num=1
+    if num>paginator.num_pages:
+        num=paginator.num_pages
+    start=((num-1)/2)*2+1
+    end=start+2
+    if end>paginator.num_pages:
+        end=paginator.num_pages+1
+    # print(range(start,end))
+    return paginator.page(num),range(start,end)
+def c_manage(request,num='1'):
+    # print num
+    size=2
+    posts,page_range=page(num,size)
+    # print posts
+    # cate_post=Post.objects.values('category__cname','category').annotate(c=Count('*')).order_by('-c')
+    return render(request,'c_manage.html',{'posts':posts,'page_range':page_range})
 
-def c_manage(request):
-    return render(request,'c_manage.html')
+
+
+
+def c_news(request):
+    return render(request,'c_news.html')
 
 
 def c_classify(request):
     return render(request,'c_classify.html')
+
+
+def post_view(request,postId):
+    post=Post.objects.get(id=postId)
+    return render(request,'post.html',{'post':post})
