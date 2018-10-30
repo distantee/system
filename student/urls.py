14a88a413edd812fb20f6bdd103b3823bbb8a383
xiaol <2757464202@qq.com>
2018-10-30 19:57:49 +0800

    版本5.4添加修改课程功能,添加相关注释

--- student/urls.py
+++ student/urls.py
@@ -6,26 +6,33 @@ from django.views.static import serve
 from student import views
 
 urlpatterns = [
+    #首页路由
     url(r'^index/',views.index_view,name='index'),
-    url(r'^all_menus/',views.all_meuns),
-
+   #内容管理路由
     url(r'^c_manage/(\d+)', views.c_manage,name='c_manage'),
-    #详情页路由
+    #页面详情信息的路由
     url(r'^post/(\d+)', views.post_view,name='post'),
+    #近期要闻路由
     url(r'^c_news/', views.c_news),
+    #简要分类的路由
     url(r'^c_simply/', views.c_simply),
+    #时间分类的主页面路由
     url(r'^c_time/', views.c_time),
+    #时间详情页表路由
     url(r'^archive/(\d+)/(\d+)', views.archive_view),
     # url(r'^media/(?P<path>.*)/$', serve, {"document_root": MEDIA_ROOT})
-
-    url(r'^grade/',views.grade_view,name='grade'),
     url(r'^gradecx/',views.gradecx_view),
     url(r'^gradelr/',views.gradelr_view),
     url(r'^delete/(\d+)',views.deletegrade_view),
-    url(r'^course/',views.course_view,name='course'),
+    #添加课程信息
     url(r'^addCourse/',views.addCourse_view,name='addCourse'),
+    #展示课程信息
     url(r'^showCourse/',views.showCourse_view,name='showCourse'),
+    #课程操作
     url(r'^operCourse/',views.operCourse_view,name='operCourse'),
+    #修改课程信息
+    url(r'^modifCourse/(\d+)',views.modifCourse_view,name='modifCourse'),
+    #删除课程信息
     url(r'^delCourse/(\d+)',views.delCourse_view,name='delCourse'),
     url(r'^show/(\d+)',views.show_view,name='show'),
     url(r'^register/',views.register_view,name='register'),
--- student/views.py
+++ student/views.py
@@ -8,40 +8,49 @@ from django.shortcuts import render, redirect
 from .models import *
 
 # Create your views here.
-
+#展示多个课程的相关信息
 def showAll(courses):
     # courses = TCourse.objects.all()
+    #用来存放每个课程的相关信息
     messages = []
+    #得到每门课程与其选课人数的相关信息
     set1 = TStuentCourse.objects.values('course_id').annotate(c=Count('*'))
+
     for course in courses:
+        #获取该课程对应的所有教师信息，TTeacherCourse对象
         allteachers = TTeacherCourse.objects.filter(courseid=course.courseid)
+        #存放所有教师信息，TTeacher对象
         teachers = []
         for teacher in allteachers:
+            #转化TTeacherCourse对象为TTeacher对象
             teacher = TTeacher.objects.get(teacherid=teacher.teacherid_id)
             teachers.append(teacher)
         xkcourses = []
         for xkgx in set1:
+            #得到课程id
             xkcourses.append(xkgx['course_id'])
+            #得到选课人数
             c = xkgx['c']
+            #所得课程与课程选课人数信息之间的相互匹配
+            #匹配
             if xkgx['course_id'] == course.courseid:
+                #将课程，对应教师，选课人数存入一个列表，再存入messages大列表
                 mes = [course, teachers, c]
                 messages.append(mes)
+        #如果不匹配，则选课人数直接存为0
         if course.courseid not in xkcourses:
             mes = [course, teachers, 0]
             messages.append(mes)
     return messages
 
 
-
+#展示首页相关的信息
 def index_view(request):
     return render(request,'index.html')
 
-
-def all_meuns(request):
-    return render(request,'all_menus.html')
-
-
+#展示内容管理的相关信息
 def getpage(num,size):
+    #调用分页函数进行分页
     posts=Paginator(Post.objects.order_by('-created'),size)
     if num<=0:
         num=1
@@ -57,38 +66,30 @@ def c_manage(request,num='1'):
     posts,page_range=getpage(num,3)
     return render(request, 'c_manage.html', {'posts': posts,'page_range':page_range})
 
-
-
-
+#展示近期要闻的相关信息
 def c_news(request):
     return render(request,'c_news.html')
 
-
+#展示简要分类的相关信息
 def c_simply(request):
     c_post=Post.objects.all()
     return render(request, 'c_simply.html',{'c_post':c_post})
 
+#展示时间分类表的页面
 def c_time(request):
     return render(request, 'c_time.html')
 
-
+#展示详情页的信息
 def post_view(request,postId):
     post=Post.objects.get(id=postId)
     return render(request,'post.html',{'post':post})
 
-
+#按照时间分类表分类出来的表的相关信息
 def archive_view(request,year,month):
     # print year,month
     c_post = Post.objects.filter(created__year=year, created__month=month)
     # print c_post
     return render(request,'archive.html',{'c_post':c_post})
-
-
-def grade_view(request):
-    grades = Grade.objects.all()
-    #print grades
-    return render(request,'grade.html',{'grades':grades})
-
 
 def gradecx_view(request):
     if request.method == 'GET':
@@ -147,7 +148,6 @@ def deletegrade_view(request,gradeid):
     grade = Grade.objects.get(gradeid=gradeid)
     grade.delete()
     return HttpResponse('删除成功')
-
 
 def  register_view(request):
     if request.method == 'GET':
@@ -305,36 +305,43 @@ def del1_view(request,delid):
     stu.delete()
     return redirect('/student/operate/')
 
-def course_view(request):
-    return render(request,'course.html')
-
+#添加课程功能
 def addCourse_view(request):
+    #初次进入页面传入已有的教师信息
     if request.method=='GET':
         teachers = TTeacher.objects.all()
         return render(request,'addCourse.html',{'teachers':teachers})
     else:
+        #获取参数
         name=request.POST.get('coursename','')
+        #得到的教师列表第一项是输入框的内容，后面的是复选框中的内容
         reTnames=request.POST.getlist('rkTname','')
+        #添加课程到课程表
         try:
             course=TCourse.objects.get(coursename=name)
         except TCourse.DoesNotExist:
             course=TCourse.objects.create(coursename=name)
 
+        #存储所选的所有教师
         tnames=[]
+        #如果有输入新教师，则遍历所有
         if reTnames[0]:
             for reTname in reTnames:
+                #存储教师信息到教师表
                 try:
                     tname=TTeacher.objects.get(teachername=reTname)
                 except TTeacher.DoesNotExist:
                     tname=TTeacher.objects.create(teachername=reTname)
                 tnames.append(tname)
 
+            #存储课程信息和对应的教师信息到教师任课表中
             for tname in tnames:
                 # print tname,course
                 try:
                     TTeacherCourse.objects.get(teacherid=tname.teacherid,courseid=course.courseid)
                 except TTeacherCourse.DoesNotExist:
                     TTeacherCourse.objects.create(teacherid=tname,courseid=course)
+        # 如果没有输入新教师，则从教师列表第二项开始遍历所有
         else:
             for reTname in reTnames[1:]:
                 try:
@@ -349,37 +356,55 @@ def addCourse_view(request):
                     TTeacherCourse.objects.get(teacherid=tname.teacherid, courseid=course.courseid)
                 except TTeacherCourse.DoesNotExist:
                     TTeacherCourse.objects.create(teacherid=tname, courseid=course)
+            #添加成功，跳转到课程展示页面
         return redirect('/student/showCourse/')
-
+#展示课程功能
 def showCourse_view(request):
+    #获取所有课程
     courses = TCourse.objects.all()
+    #调用展示函数
     messages=showAll(courses)
-
+    #分页操作
     num = request.GET.get('num', 1)
     num = int(num)
-    paginator = Paginator(messages,3)
-    try:
-        t_pre_page = paginator.page(num)  # 获取当前页码的记录
-    except PageNotAnInteger:  # 如果用户输入的页码不是整数时,显示第1页的内容
-        t_pre_page = paginator.page(1)
-    except EmptyPage:  # 如果用户输入的页数不在系统的页码列表中时,显示最后一页的内容
-        t_pre_page = paginator.page(paginator.num_pages)
+    per = 3
+    pages = Paginator(messages,per)
+    if num <= 0:
+        num = 1
+    if num > pages.num_pages:
+        num = pages.num_pages
+    # 生成页码数
+    start = num
+    end = num + 3
+    # 判断end是否越界
+    if end > pages.num_pages:
+        end = pages.num_pages + 1
+    messages=pages.page(num)
+    page_range=range(start, end)
 
-    return render(request,'showCourse.html',{'paginator':paginator,'t_pre_page':t_pre_page})
-
+    return render(request,'showCourse.html',{'messages':messages,'page_range':page_range})
+#课程操作功能
 def operCourse_view(request):
     if request.method=='GET':
+        #初次进入该页面传入所有信息，与展示页面相同
         courses = TCourse.objects.all()
         messages = showAll(courses)
         return render(request, 'operCourse.html',{'messages':messages})
     else:
+        #得到查询条件
         key=request.POST.get('key','')
+        #约束条件
         constraint=request.POST.get('constraint','')
+        #值
         val=request.POST.get('val','')
         # print key,constraint,val
+        #课程编号
         if key=='cid':
+            #大于
             if constraint=='gt':
+                #得到大于值的所有课程信息
                 courses=TCourse.objects.filter(courseid__gt=val)
+                # 调用展示函数
                 messages=showAll(courses)
             elif constraint=='lt':
                 courses = TCourse.objects.filter(courseid__lt=val)
@@ -387,8 +412,11 @@ def operCourse_view(request):
             else:
                 courses = TCourse.objects.filter(courseid=val)
                 messages = showAll(courses)
+        #课程名称
         elif key=='cname':
-            course=TCourse.objects.get(coursename=val)
+            # 可模糊查询，有关键字即可找到
+            course=TCourse.objects.get(coursename__contains=val)
+            # 展示
             messages = []
             set1 = TStuentCourse.objects.values('course_id').annotate(c=Count('*'))
             allteachers = TTeacherCourse.objects.filter(courseid=course.courseid)
@@ -406,21 +434,77 @@ def operCourse_view(request):
             if course.courseid not in xkcourses:
                 mes = [course, teachers, 0]
                 messages.append(mes)
+        #教师名称
         else:
-            all=TTeacherCourse.objects.filter(teacherid__teachername__contains=val)
+            all=TTeacherCourse.objects.filter(teacherid__teachername=val)
             # print all
             courses=[]
+            #获取该教师所授的所有课程
             for single in all:
                 course=TCourse.objects.get(courseid=single.courseid_id)
                 courses.append(course)
             messages = showAll(courses)
         return render(request, 'operCourse.html', {'messages': messages})
-
+#删除课程功能
 def delCourse_view(request,courseid):
     # print courseid
+    #首先删除教师任课中间表中的该课程相关记录
     TTeacherCourse.objects.filter(courseid=courseid).delete()
+    #删除创建表中与该课程相关的记录
     Grade.objects.filter(courseid=courseid).delete()
+    #删除学生选课表中与该课程相关的记录
     TStuentCourse.objects.filter(course=courseid).delete()
+    #删除课程表中该课程信息
     TCourse.objects.get(courseid=courseid).delete()
 
     return redirect('/student/showCourse/')
+#修改课程功能
+def modifCourse_view(request,courseid):
+    if request.method=='GET':
+        #初次进入页面，默认展示该课程的所有信息
+        course = TCourse.objects.get(courseid=courseid)
+        messages = []
+        set1 = TStuentCourse.objects.values('course_id').annotate(c=Count('*'))
+        allteachers = TTeacherCourse.objects.filter(courseid=course.courseid)
+        teachers = []
+        exceptTeacher=TTeacher.objects.all()
+        for teacher in allteachers:
+            teacher = TTeacher.objects.get(teacherid=teacher.teacherid_id)
+            teachers.append(teacher)
+            #获取所有除了该课程原来任教的教师的教师信息
+            exceptTeacher=exceptTeacher.exclude(teacherid=teacher.teacherid)
+        xkcourses = []
+        for xkgx in set1:
+            xkcourses.append(xkgx['course_id'])
+            c = xkgx['c']
+            if xkgx['course_id'] == course.courseid:
+                mes = [course, teachers, c]
+                messages.append(mes)
+        if course.courseid not in xkcourses:
+            mes = [course, teachers, 0]
+            messages.append(mes)
+        courses=TCourse.objects.all()
+
+        return render(request,'modifCourse.html',{'messages':messages,'courses':courses,'exceptTeacher':exceptTeacher})
+    else:
+        # print courseid
+        #先删除教师任课表中原有的该课程相关数据
+        TTeacherCourse.objects.filter(courseid=courseid).delete()
+        #接收参数
+        coursename=request.POST.get('coursename','')
+        rkTeachers=request.POST.getlist('rkTeacher','')
+        # print coursename,rkTeacher,xknum
+        #修改课程名称并存储
+        cour=TCourse.objects.get(courseid=courseid)
+        cour.coursename=coursename
+        cour.save()
+        for rkTeacher in rkTeachers:
+            # 若任课教师非空
+            if rkTeacher:
+                try:
+                    teac=TTeacher.objects.get(teachername=rkTeacher)
+                except TTeacher.DoesNotExist:
+                    teac=TTeacher.objects.create(teachername=rkTeacher)
+                #将课程与教师的相关信息存入教师任课表
+                TTeacherCourse.objects.create(courseid=cour,teacherid=teac)
+        return redirect('/student/showCourse/')
